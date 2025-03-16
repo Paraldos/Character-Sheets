29d19427b20d6908620ee3e44f9e6036a8902cd8
Paraldos <hillbrecht.s@gmail.com>
2025-03-16 16:45:01 +0100

    Base Setup Left Page

--- src/diablo/diablo.jsx
+++ src/diablo/diablo.jsx
@@ -6,9 +6,43 @@ import RightPage from "./rightPage/RightPage";
 function Diablo() {
   return (
     <Page className={"page__landscape diablo"}>
-      <LeftPage>Left</LeftPage>
+      <LeftPage>
+        <h2>Basic Infos</h2>
+        <ul>
+          <li>Name</li>
+          <li>Rasse</li>
+          <li>Hintergrund</li>
+          <li>Gesinnung</li>
+        </ul>
+        <h2>Attribute</h2>
+        <ul>
+          <li>Attribut 1</li>
+          <li>Attribut 2</li>
+          <li>Attribut 3</li>
+          <li>Attribut 4</li>
+          <li>Attribut 5</li>
+          <li>Attribut 6</li>
+        </ul>
+        <h2>Fertigkeiten</h2>
+        <ul>
+          <li>Fertigkeit 1</li>
+          <li>Fertigkeit 2</li>
+          <li>Fertigkeit 3</li>
+          <li>Fertigkeit 4</li>
+          <li>Fertigkeit 5</li>
+          <li>Fertigkeit 6</li>
+        </ul>
+        <h2>Sprachen</h2>
+        <h2>Ausrüstung</h2>
+        <h2>Merkmale</h2>
+        <ul>
+          <li>TP</li>
+          <li>MP</li>
+          <li>RK</li>
+        </ul>
+      </LeftPage>
       <RightPage>
-        <h1>Playbook Title</h1>
+        <h1>Playbook Title + Stufe</h1>
         <p>Playbook Description</p>
         <h2>Talente</h2>
         <ul>
