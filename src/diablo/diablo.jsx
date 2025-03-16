8fcd061303b155024291049619bf6a4171e54799
Paraldos <hillbrecht.s@gmail.com>
2025-03-16 16:41:08 +0100

    Base structure right page

--- src/diablo/diablo.jsx
+++ src/diablo/diablo.jsx
@@ -7,7 +7,22 @@ function Diablo() {
   return (
     <Page className={"page__landscape diablo"}>
       <LeftPage>Left</LeftPage>
-      <RightPage>Right</RightPage>
+      <RightPage>
+        <h1>Playbook Title</h1>
+        <p>Playbook Description</p>
+        <h2>Talente</h2>
+        <ul>
+          <li>Talent 1</li>
+          <li>Talent 2</li>
+          <li>Talent 3</li>
+        </ul>
+        <h2>Manöver</h2>
+        <ul>
+          <li>Manöver 1</li>
+          <li>Manöver 2</li>
+          <li>Manöver 3</li>
+        </ul>
+      </RightPage>
     </Page>
   );
 }
