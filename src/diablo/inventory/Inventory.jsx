60e575ab521bd08dbf6e9fdb930febb5386909ec
Paraldos <hillbrecht.s@gmail.com>
2025-03-16 18:15:04 +0100

    kleinere styling anpassungen

--- src/diablo/inventory/Inventory.jsx
+++ src/diablo/inventory/Inventory.jsx
@@ -1,21 +1,15 @@
 import "./inventory.css";
 
 function Inventory() {
+  const list_of_slots = Array.from({ length: 12 });
+
   return (
     <div className="inventory">
       <h3>Gepäck (5 + Stärke Slots)</h3>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        {list_of_slots.map((_, index) => (
+          <li key={index}></li>
+        ))}
       </ul>
     </div>
   );
