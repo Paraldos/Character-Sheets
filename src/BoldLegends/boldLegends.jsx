9b28ded04dd9a4299563b2ddd2027033868555f0
Paraldos <hillbrecht.s@gmail.com>
2025-05-13 18:08:28 +0200

    added title

--- src/BoldLegends/boldLegends.jsx
+++ src/BoldLegends/boldLegends.jsx
@@ -1,7 +1,11 @@
 import "./boldLegends.css";
 
 function BoldLegends() {
-  return <Page className={"boldLegends"}></Page>;
+  return (
+    <Page className={"boldLegends"}>
+      <h1>Bold Legends</h1>
+    </Page>
+  );
 }
 
 export default BoldLegends;
