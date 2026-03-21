e9196078e97d91ae6c4cc7fc6b05b66b95d61630
Schiggy <hillbrecht.s@gmail.com>
2026-03-21 08:52:59 +0100

    basic setup

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+const app = document.getElementById("app");
+
+app.innerHTML = "BERT";
+console.log(app);
--- src/page/page.jsx
+++
@@ -1,7 +0,0 @@
-import "./page.css";
-
-function Page({ children, className }) {
-  return <div className={`page ${className}`}>{children}</div>;
-}
-
-export default Page;
