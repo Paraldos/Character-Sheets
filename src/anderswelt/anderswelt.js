8a714a3c30d55af22b63b9c413b7d5ac72fd8377
Schiggy <hillbrecht.s@gmail.com>
2026-03-26 19:35:28 +0100

    removed mäander

--- src/anderswelt/anderswelt.js
+++ src/anderswelt/anderswelt.js
@@ -2,20 +2,16 @@ import Page from "../page";
 import "./anderswelt.css";
 import Basics from "./basics";
 import Fertigkeiten from "./fertigkeiten";
-import Maeander from "./maeander";
 
 export default class Anderswelt extends Page {
   constructor(container) {
     super(container);
     this.page.classList.add("anderswelt");
-    new Maeander(this.page);
     new Basics(this.page);
-    new Maeander(this.page);
 
     const e = document.createElement("div");
     e.className = "anderswelt-bottom";
     this.page.appendChild(e);
-
     new Fertigkeiten(e);
   }
 }
