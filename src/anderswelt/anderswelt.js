3544598e697a1e1cdccbb15c67c7b546a12a1e5c
Schiggy <hillbrecht.s@gmail.com>
2026-03-27 15:45:16 +0100

    refined playbooks

--- src/anderswelt/anderswelt.js
+++ src/anderswelt/anderswelt.js
@@ -31,13 +31,12 @@ export default class Anderswelt extends Page {
         {
           titel: "Einfallsreich",
           cost: "Stress",
-          description:
-            "Improvisierte Werkzeuge geben dir einen +2 Bonus (anstelle von +1)",
+          description: "Improvisierte Werkzeuge geben +2 anstelle von +1.",
         },
         {
           titel: "Fit",
           cost: "Stress",
-          description: "+2 auf eine Athletik- oder Akrobatikprobe.",
+          description: "+2 auf Athletik- oder Akrobatik-Proben.",
         },
         {
           titel: "Goldnase",
@@ -53,18 +52,18 @@ export default class Anderswelt extends Page {
         },
         {
           titel: "Kletteraffe",
-          description: "+2 auf Klettern und Balancieren.",
+          description: "+2 auf Klettern- und Balancieren-Proben.",
         },
         {
           titel: "Scharfschütze",
           description:
-            "+2 auf Attacken mit Jagdwaffen, wenn du dich nicht bewegst.",
+            "+2 auf Attacken mit Jagdwaffen, wenn du dich diese Runde nicht bewegst.",
         },
         {
           titel: "Scout",
           cost: "Stress",
           description:
-            "Erhalte +2 auf eine Probe für Orientierung, Wahrnehmung oder Wildnisleben.",
+            "+2 auf Orientierung-, Wahrnehmung- oder Wildnisleben-Probe.",
         },
         {
           titel: "Zäh",
@@ -88,14 +87,67 @@ export default class Anderswelt extends Page {
         "Verführerischer Manipulator, der über Leichen geht, um seine Ziele zu erreichen.",
       items: [
         { name: "Pistole" },
-        { name: "Gift" },
-        { name: "Elegantes Outfit" },
         { name: "Dietriche" },
+        { name: "Elegantes Outfit" },
+        { name: "Gift" },
+        { name: "Schlafmittel" },
         { name: "Falsche Papiere" },
         { name: "Zigaretten" },
         { name: "Bargeld", amount: 3 },
       ],
-      talents: [],
+      talents: [
+        {
+          titel: "Assassine",
+          description: "+1 Schaden gegen unvorbereitete Gegner.",
+        },
+        {
+          titel: "Eiskalt",
+          cost: "Stress",
+          description: "+2 auf Einschüchtern-Probe.",
+        },
+        {
+          titel: "Flinke Finger",
+          cost: "Stress",
+          description:
+            "+2 auf Diebstahl- oder Falschspiel-Probe (z.B. um Gift unterzumischen).",
+        },
+        {
+          titel: "Gutaussehend",
+          description: "+2 auf Verführen-Proben.",
+        },
+        {
+          titel: "Harmlos",
+          description:
+            "Du wirst generell unterschätzt und im Kampf nur attackiert, wenn kein anderes Ziel zur Verfügung steht.",
+        },
+        {
+          titel: "Klatsch",
+          cost: "Urbane Umgebung, eine Stunde",
+          description:
+            "Der Erzähler beantwortet dir 3 soziale oder politische Ja/Nein-Fragen.",
+        },
+        {
+          titel: "Kontakte",
+          cost: "Urbane Umgebung, eine Stunde",
+          description: "Du treibst jemanden auf, der helfen kann.",
+        },
+        {
+          titel: "Netzwerk",
+          cost: "Urbane Umgebung, eine Stunde",
+          description: "Du erhältst Zugang zu einem exklusiven Ort oder Kreis.",
+        },
+        {
+          titel: "Silberzunge",
+          cost: "Stress",
+          description: "+2 auf Überreden-, Manipulieren- oder Lügen-Probe.",
+        },
+        {
+          titel: "Entertainer",
+          cost: "Stress",
+          description:
+            "+2 auf Unterhaltungsproben (Singen, Tanzen etc.). Hält für eine Szene.",
+        },
+      ],
     },
     ganove: {
       titel: "Ganove",
@@ -114,34 +166,34 @@ export default class Anderswelt extends Page {
       ],
       talents: [
         {
-          titel: "Ablenken",
-          cost: "Aktion, Stress",
-          description: "Alle attacken gegen Ziel erhalten für eine Runde +2.",
-        },
-        {
           titel: "Allerweltsgesicht",
           description:
             "Du wirst meist ignoriert und bleibst kaum in Erinnerung.",
         },
         {
           titel: "Assassine",
-          description:
-            "+1 Schaden gegen abgelenkte oder unvorbereitete Gegner.",
+          description: "+1 Schaden gegen unvorbereitete Gegner.",
         },
         {
           titel: "Fasttalker",
           cost: "Stress",
-          description: "+2 auf eine Probe zum Lügen oder Herausreden.",
+          description: "+2 auf Lügen- oder Herausreden-Probe.",
         },
         {
           titel: "Finte",
-          cost: "Aktion, Stress",
-          description: "Führe eine Attacke mit +2 aus.",
+          cost: "Stress",
+          description: "+2 auf Attacke-Probe.",
         },
         {
           titel: "Flinke Finger",
           cost: "Stress",
-          description: "+2, um Schloss oder Falle in Sekunden zu knacken.",
+          description: "+2 auf Diebstahl- oder Falschspiel-Probe.",
+        },
+        {
+          titel: "Gassenwissen",
+          cost: "Urbane Umgebung, eine Stunde",
+          description:
+            "Der Erzähler beantwortert dir 3 Ja/Nein-Fragen zur kriminellen Unterwelt.",
         },
         {
           titel: "Glück",
@@ -150,24 +202,25 @@ export default class Anderswelt extends Page {
         },
         {
           titel: "Kontakte",
-          cost: "Einmal pro Abend, Urbane Umgebung",
-          description: "Du kennst hier jemanden, der helfen kann.",
+          cost: "Urbane Umgebung, eine Stunde",
+          description: "Du treibst jemanden auf, der helfen kann.",
         },
         {
           titel: "Rauchbombe",
           cost: "Aktion, Stress",
-          description: "Umstehende Gegner müssen eine Runde aussetzen.",
+          description:
+            "Umstehende Gegner sind verwirrt und müssen eine Runde aussetzen.",
         },
         {
           titel: "Schatten",
           cost: "Stress",
           description:
-            "+2 auf Heimlichkeit. Hält solange du unauffällig bleibst.",
+            "+2 auf Heimlichkeit. Hält solange du dich unauffällig verhältst.",
         },
         {
-          titel: "Trickser",
+          titel: "Schlossmeister",
           cost: "Stress",
-          description: "+2 auf eine Diebstahl oder Falschspiel Probe.",
+          description: "+2 auf Schlösser- oder Fallen-Proben.",
         },
       ],
     },
